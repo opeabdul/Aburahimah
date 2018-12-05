2b84923e289f427dd5d9b73306f4fe08a3a9d2c6
southside <opdort24@gmail.com>
2018-12-05 11:26:17 +0100

    initial commit

---
+++ app/src/main/java/com/example/opeyemi/aburahimah/ChaptersActivity.java
@@ -0,0 +1,13 @@
+package com.example.opeyemi.aburahimah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChaptersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapters);
+    }
+}
